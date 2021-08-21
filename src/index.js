004b1e603bdd8cee0b9ef64eccce87ff9f88586e
AndrewSeliverstov <aardvarkd101@gmail.com>
2021-08-21 11:41:44 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    
+        let digits = [];
+        
+        let reversedNum = 0;
+    
+        while (n){
+          digits.push(n % 10);
+          n = Math.floor(n/10);     
+        }
+    
+        let reversedStr = digits.join('');
+        reversedNum = Number(reversedStr);
+        return reversedNum;
+};
